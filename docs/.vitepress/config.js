50631344b8c6f5ed3d700e4b0fa563971fcbd9dc
clark-cui <cuirongchuan@163.com>
2021-02-10 10:31:16 +0800

    update vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,48 +5,49 @@ module.exports = {
     lineNumbers: true,
   },
   head: [
-    ["link", {
-      rel: "icon",
-      href: "/favicon.ico"
-    }],
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
     [
       "meta",
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        content:
+          "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
       },
     ],
   ],
   themeConfig: {
-    nav: [{
+    nav: [
+      {
         text: "Home",
-        link: "/"
+        link: "/",
       },
 
       {
         text: "frontEnd",
-        link: "/frontEnd/"
+        link: "/frontEnd/",
       },
       {
         text: "backEnd",
-        link: "/backEnd/"
+        link: "/backEnd/",
       },
     ],
 
     blogConfig: {
-     
-      socialLinks: [
-        { link: 'https://github.com/clark-cui' },
-      ]
+      socialLinks: [{ link: "https://github.com/clark-cui" }],
     },
-    author: 'clark-cui',
+    author: "clark-cui",
     // 备案号
-    record: 'ICP备案号',
-    recordLink: 'http://www.baidu.com',
-    cyberSecurityRecord: '公安备案号',
-    cyberSecurityLink: 'http://www.baidu.com',
+    record: "ICP备案号",
+    recordLink: "http://www.baidu.com",
+    cyberSecurityRecord: "公安备案号",
+    cyberSecurityLink: "http://www.baidu.com",
     // 项目开始时间
-    startYear: '2018',
-
+    startYear: "2018",
   },
 };
